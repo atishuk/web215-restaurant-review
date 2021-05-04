2de520e006c6f1303023ea2d92d3e1a126d13389
ntsishu0 <54260758+ntsishu0@users.noreply.github.com>
2021-05-04 19:08:24 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
-const uri = process.env.MONGODB_URI;
 const app = express()
 
 app.use(cors())
